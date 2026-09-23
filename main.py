f8426ada1a044699465ebdc3ca16b4d0c75e7c4b
jpskol <jessicaskolnick05@gmail.com>
2026-09-23 11:20:49 -0400

    Datagen Try
    the first try of making the datagen

---
+++ main.py
@@ -0,0 +1,10 @@
+import src.datagen as datagen
+
+def main() -> None:
+    print("Hello from projectpenney-jps-eks!")
+    datagen.main()
+    print("The decks are ')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/datagen.py
@@ -0,0 +1,80 @@
+# Based on our class example?
+
+import numpy as np
+PATH_DECKS = Path('data/decks/')
+PATH_SEED_LOG = Path('data/seed.json')
+SEED_BASE = 1
+
+def gen_decks(seed: int, 
+               n_decks: int,
+              ) -> np.ndarray:
+    
+    rng = np.random.default_rng(seed)
+
+    deck = np.array([1]*26 + [0]*26)
+    decks = np.empty((n_decks, 52), dtype = int)
+
+    for _ in range(n_decks):
+        decks[i] = rng.permutation(deck)
+        
+    return rng.integers(size=(n_decks))
+
+
+def get_next_seed() -> int:
+    '''
+    Read the last seed used, increment by 1,
+    and update seed.json.
+    '''
+    # Make sure the parent directory(s) exists
+    PATH_SEED_LOG.parent.mkdir(parents=True, exist_ok=True)
+
+    # Determine the next seed
+    if not PATH_SEED_LOG.exists():
+        print(f'No seed log found, starting with {SEED_BASE}')
+        seed = SEED_BASE
+    else:
+        with PATH_SEED_LOG.open('r') as f:
+            seed_log = json.load(f)
+        seed = seed_log['seed'] + 1
+
+    # Update the log
+    seed_log = {
+        'seed': seed,
+        'seed_time': str(dt.now())
+    }
+    with PATH_SEED_LOG.open('w') as f:
+        json.dump(seed_log, f)
+    
+    return seed
+
+
+def save_decks(decks: np.ndarray, 
+               seed: int
+              ) -> Path:
+    '''
+    This doesn't actually save anything,
+    it is just a demo of how I might construct
+    the filename.
+    '''
+    PATH_DECKS.mkdir(parents=True, exist_ok=True)
+
+    n_decks = decks.shape[0]
+
+    packed = np.packbits(decks, axis = 1)
+
+    filename = PATH_DECKS / f'decks_{n_decks}_seed_{seed}.bin'
+
+    packed.tofile(filename)
+    
+    print(f'I might save this file like: {filename}')
+    return filename
+
+
+
+
+
+
+
+
+
+    
